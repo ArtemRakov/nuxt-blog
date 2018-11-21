85f0fadd2f6775858ccb28936322b68bcc74a25f
Artem Rakov <scoff1k@gmail.com>
2018-11-21 16:20:02 +0300

    finished section 3

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,84 @@
+<template>
+   <div class="home-page">
+    <section class="featured-posts">
+      <PostPreview
+          id="1"
+          thumbnail="https://www.dreamhost.com/blog/wp-content/uploads/2016/08/DreamHost-Top-Tech-Trends.jpg"
+          :is-admin="isAdmin"
+          title="Helle there!"
+          previewText="This is my first post" />  
+        <PostPreview
+          id="2"
+          thumbnail="https://www.dreamhost.com/blog/wp-content/uploads/2016/08/DreamHost-Top-Tech-Trends.jpg"
+          :is-admin="isAdmin"
+          title="Hi"
+          previewText="This is my second post" />    
+        <PostPreview
+          id="3"
+          thumbnail="https://www.dreamhost.com/blog/wp-content/uploads/2016/08/DreamHost-Top-Tech-Trends.jpg"
+          :is-admin="isAdmin"
+          title="Helle there!"
+          previewText="This is my third post" />  
+    </section>
+    </div>
+</template>
+
+<script>
+import PostPreview from '~/components/Posts/PostPreview'
+
+export default {
+    components:  {
+      PostPreview
+    },
+    props: {
+      isAdmin: {
+        type: Boolean,
+        default: false
+      }
+    }    
+}
+</script>
+
+
+<style scoped>
+.intro {
+height: 300px;
+position: relative;
+padding: 30px;
+background-image: url('~assets/images/main-page-background.jpg');
+box-sizing: border-box;
+background-position: center;
+background-size: cover;
+}
+
+.intro h1 {
+position: absolute;
+top: 10%;
+left: 5%;
+width: 90%;
+font-size: 1.5rem;
+color: black;
+background-color: rgb(211, 211, 211);
+padding: 10px;
+border-radius: 10px;
+box-shadow: 3px 3px 3px black;
+box-sizing: border-box;
+border: 1px solid black;
+}
+
+@media(min-width: 768px) {
+.intro h1 {
+font-size: 2rem;
+}
+}
+
+.featured-posts {
+display: flex;
+padding: 20px;
+box-sizing: border-box;
+flex-wrap: wrap;
+align-items: center;
+justify-content: center;
+}
+
+</style>
