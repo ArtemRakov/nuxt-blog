a8ddc0968dd83c47df50c3a3a7b58da0fb9363f8
Artem Rakov <scoff1k@gmail.com>
2018-12-25 09:26:12 +0300

    finished section 7

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,6 +2,7 @@
   <div class="admin-page">
     <section class="new-post">
       <AppButton @click="$router.push('/admin/new-post')"> Create Post </AppButton>
+      <AppButton @click="onLogout"> Logout </AppButton>
     </section>
 
     <section class="existing-posts">
@@ -16,9 +17,16 @@
 
 export default {
   layout: 'admin',
+  middleware: ['check-auth', 'auth'],
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts
+    }
+  },
+  methods: {
+    onLogout() {
+      this.$store.dispatch('logout')
+      this.$router.push('/admin/auth')
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex'
 import axios from 'axios'
+import Cookie from 'js-cookie'
 
 const createStore = () => {
   return new Vuex.Store({
@@ -22,6 +23,9 @@ const createStore = () => {
       },
       setToken(state, token) {
         state.token = token
+      },
+      clearToken(state) {
+        state.token = null
       }
     },
     actions: {
@@ -44,7 +48,7 @@ const createStore = () => {
           ...post, 
           updateDate: new Date 
         }
-        return axios.post('https://nuxt-blog-7be92.firebaseio.com/posts.json', createdPost)
+        return axios.post('https://nuxt-blog-7be92.firebaseio.com/posts.json?auth=' + vuexContext.state.token, createdPost)
           .then(result => {
 
             vuexContext.commit('addPost', { ...createdPost, id: result.data.name})
@@ -52,7 +56,7 @@ const createStore = () => {
           .catch(e => console.log(e))
       },
       editedPost(vuexContext, editedPost) {
-        return axios.put('https://nuxt-blog-7be92.firebaseio.com/posts/' + editedPost.id + '.json', editedPost)
+        return axios.put('https://nuxt-blog-7be92.firebaseio.com/posts/' + editedPost.id + '.json?auth=' + vuexContext.state.token, editedPost)
           .then(res => {
             vuexContext.commit('editPost', editedPost)
           })
@@ -71,13 +75,63 @@ const createStore = () => {
           }
         ).then(result => {
           vuexContext.commit('setToken', result.data.idToken)
+          localStorage.setItem('token', result.data.idToken)
+          localStorage.setItem('tokenExpiration', new Date().getTime() + Number.parseInt(result.data.expiresIn) * 1000)
+          Cookie.set('jwt', result.data.idToken)
+          Cookie.set('expirationDate', new Date().getTime() + Number.parseInt(result.data.expiresIn) * 1000)
+
         })
           .catch(e => console.warn('Auth Error MSG: ', e.response.data.error.message)) 
+      },
+      initAuth(vuexContext, req) {
+        let token;
+        let expirationDate;
+      
+        if (req) {
+          if (!req.headers.cookie) {
+            return;
+          }
+          const jwtCookie = req.headers.cookie.split(';')
+          .find(c => c.trim().startsWith('jwt='))
+
+          if (!jwtCookie) {
+            return;
+          }
+          token = jwtCookie.split('=')[1];
+          expirationDate = req.headers.cookie.split(';')
+            .find(c => c.trim().startsWith('expirationDate='))
+            .split("=")[1]
+        }
+        else {
+          token = localStorage.getItem('token')
+          expirationDate = localStorage.getItem('tokenExpiration')
+
+        }
+
+        if (new Date().getTime() > +expirationDate || !token) {
+          vuexContext.dispatch('logout');
+          return;
+        }
+        vuexContext.commit('setToken', token)
+        
+      },
+      logout(vuexContext) {
+        vuexContext.commit('clearToken')
+        Cookie.remove('jwt')
+        Cookie.remove('expirationDate')
+
+        if (process.client) {
+          localStorage.removeItem('token')
+          localStorage.removeItem('tokenExpiration')
+        }
       }
     },
     getters: {
       loadedPosts(state) {
         return state.loadedPosts
+      },
+      isAuthenticated(state) {
+        return state.token != null
       }
     }
   })
